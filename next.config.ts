27ff384821eb6d97b13c6b71024cd6bc4f352c58
Ammarasaif <159964042+Ammarasaif@users.noreply.github.com>
2025-02-06 15:55:07 +0500

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,34 @@
-import type { NextConfig } from "next";
+import { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/**@type {import('next').NextConfig} */
+const nextConfig = { 
+  images: {
+    domains: ["cdn.sanity.io"],
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
+
+
+
+
+
+
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+  
+//   }
+  
+  
+
+
+// export default nextConfig;
+
+
+
+
+
+
+
+
---
+++ src/sanity-image-url.d.ts
@@ -0,0 +1,5 @@
+declare module '@sanity/image-url' {
+    const imageUrlBuilder: any;  // You can replace `any` with a more specific type if needed
+    export default imageUrlBuilder;
+  }
+  
